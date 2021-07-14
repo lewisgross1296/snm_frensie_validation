0e82b703df35f4ee2e3ac63a2cc4335d0bfbcd40
Lewis <ligross@wisc.edu>
2021-07-14 15:26:06 -0500

    removed extra example from sbatch script

--- frensie_comparison/air/air_empty/air_empty_25/snm-submit.sh
+++ frensie_comparison/air/air_empty/air_empty_25/snm-submit.sh
@@ -18,16 +18,8 @@ mpirun -np $SLURM_NTASKS singularity exec frensie_hpc.simg python snm.py --sim_n
 # -np $SLURM_NTASKS has mpi create $SLURM_NTASKS copies of the program and MPI handles the data;
 # transfering between nodes and the process 0 node 
 
+###############################################################################
 # RESOURCE EXPLANATION
 # --nodes=2 #how many nodes, each containing 20 cpus do you want
 # --ntasks-per-node=5
 # --cpus-per-task=4 # need cpus per task * ntasks per node needs to be 20 (or as close but under 20 as possible) so that we don't waste any threads/cpus (20 threads per core)
-
-###############################################################################
-# PHILIP's example
-#SBATCH --partition=cnerg
-#SBATCH --time=7-00:00:00
-#SBATCH --ntasks=40
-#SBATCH --cpus-per-task=4 ASK ABOUT THIS, IS IT FOR PERFORMANCE
-# module load openmpi
-# mpirun -np $SLURM_NTASKS singularity exec hpc_container python unbiased_forward.py --threads=$SLURM_CPUS_PER_TASK --num_particles=1e9
