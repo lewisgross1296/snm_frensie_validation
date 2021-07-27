a5b8b4d74b77432116f7d86822808dee831ae236
Lewis <ligross@wisc.edu>
2021-07-27 17:05:35 -0500

    restart script and restart batching script

---
+++ frensie_comparison/air/air_empty/air_empty_25/snm-restart-submit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --partition=pre
+#SBATCH --time=1-00:00:00 
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=5
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=600
+#SBATCH --error=snm_%J.err
+#SBATCH --output=snm_%J.out
+#SBATCH --exclude=hpc[109-112,117-136,141-168,193-208,213-256,265-292,301-316,325-332]
+# exclude nodes target hardware with AVX2 ISA
+
+
+# build command
+# FIRST LOG INTO AN INTERACTIVE NODE: srun -n1 -N1 -p int --pty bash
+# may possibly need to unset the two below environemnt variables
+# unset HTTPS_PROXY
+# unset http_proxy
+
+# BUILD SINGULARITY IMAGE FROM DOCKERHUB IMAGE
+# singularity build frensie_hpc.simg docker://ligross/frensie_hpc:frensie_stable 
+
+#BIND path to shared data in dagmc group
+BIND_PATH=/software/groups/dagmc/opt/misc/MCNP/MCNP_DATA
+module load openmpi
+mpirun -np $SLURM_NTASKS singularity exec --bind ${BIND_PATH}:${BIND_PATH} frensie_hpc.simg python snm-restart.py --rendezvous_file="snm_rendezvous_2.xml"
